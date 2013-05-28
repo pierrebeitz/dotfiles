9c4723b214ed583f597695d8efc241b4cbcffb83
Pierre Beitz <pb@naymspace.de>
2013-05-28 10:15:28 +0200

    made bootstrap dangerous as hell

--- .zshrc
+++ .zshrc
@@ -27,3 +27,6 @@ PATH=$PATH:$HOME/workspace/git-friendly
 
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+
+. $HOME/code/z/z.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,2 @@
-#!/usr/bin/env bash
-
 git pull origin master
-function doIt() {
-  rsync --exclude ".git/" --exclude "bootstrap.sh" \
-    --exclude "README.md" --exclude "install-deps.sh" \
-     -av --no-perms . ~
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-  fi
-fi
-unset doIt
-source ~/.zshrc
+rsync --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" --exclude "install-deps.sh" -av --no-perms . $HOME
