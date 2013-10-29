0f1f3dac3b849c4a56ea66ddc2c1043b133e2bd5
Pierre Beitz <pb@naymspace.de>
2013-10-29 10:44:32 +0100

    install only for user

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,6 @@
+#!/bin/bash
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 curl -L https://get.rvm.io | bash -s stable --ruby
@@ -14,13 +12,14 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 chsh -s /bin/zsh
 
+cd ~/code
 # https://github.com/rupa/z
 # z, oh how i love you
-cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
 # zsh-completions
 git clone git://github.com/zsh-users/zsh-completions.git
-
+# git-friendly
+git clone git://github.com/jamiew/git-friendly.git 
 
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
